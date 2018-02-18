fa09f2058df9156fe8ceed238ab7399d21bf4a58
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-02-17 21:49:25 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# battlesnake-python
+
+A simple [BattleSnake AI](http://battlesnake.io) written in Python. 
+
+Visit [battlesnake.io/readme](http://battlesnake.io/readme) for API documentation and instructions for running your AI.
+
+This AI client uses the [bottle web framework](http://bottlepy.org/docs/dev/index.html) to serve requests and the [gunicorn web server](http://gunicorn.org/) for running bottle on Heroku. Dependencies are listed in [requirements.txt](requirements.txt).
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+# Spanner Snake
+
+run the virtual environment using `source env/bin/activate`
+
+maybe install the packages using `pip3 install -r requirements.txt`
+
+run the snake with `python3 app/main.py`
---
+++ app/main.py
@@ -0,0 +1,75 @@
+import bottle
+import os
+import sys
+
+# Run virtual environment first
+#   source env/bin/activate
+#   You may have to pip3 install -r requirements.txt
+
+##############################################
+# WEB CALLS
+#############################################
+
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    print("STATIC request")
+    print("path={}".format(path))
+    return bottle.static_file(path, root='static')
+
+
+@bottle.post('/start')
+def start():
+    print("START request")
+    data = bottle.request.json
+
+    print("\nSNAKE START!")
+    for k,v in data.items():
+        print("{}={}".format(k,v))
+
+    print("URL Parts:")
+    print(bottle.request.urlparts)
+
+    head_url = "{}://{}/static/spanner.png".format(
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
+
+    return {
+        'color': '#FF00FF',
+        
+        
+        'name': "spanne̥̩̯̱̝̜̙̽̾̑ͯ̂̂̌r̙͇̩ͥ̓ͣ ̑̂ͦͣ̓̈́s̮̞̪̰̄͑͗ͭn̙̙̙̩̹a̘͍̍̇k̲̰̃͂e̜͚̿̂",
+
+        
+        
+        # URL of the image to display as your avatar.
+        'head_url': head_url,
+        'taunt': "Ssstarting now",
+        'head_type': 'sand-worm',
+        'tail_type': 'round-bum',
+        'secondary_color': '#00F906',
+    }
+
+
+@bottle.post('/move')
+def move():
+    print("MOVE request")
+    data = bottle.request.json
+    
+    print(data)
+
+    return {
+        'move': 'right',
+        'taunt': 'sss'
+    }
+
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+if __name__ == '__main__':
+    print("Starting bottle")
+    print(sys.version)
+
+    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '3000'))
+
